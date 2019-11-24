68ae68553e7ea7b58f8ff6485760cdfe0efb748a
Bent Tranberg <bent.tranberg@gmail.com>
2019-11-24 06:50:28 +0100

    Initial commit of solution ElmiDemos.sln

---
+++ MultiSel.Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace MultiSel.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MultiSel/Program.fs
@@ -0,0 +1,36 @@
+﻿module MultiSel.Program
+
+open System
+open Elmish
+open Elmish.WPF
+
+module App =
+
+    type Model =
+        {
+            Dummy: string
+        }
+
+    let init () =
+        {
+            Dummy = ""
+        }
+
+    type Msg =
+        | Dummy
+
+    let update msg m =
+        match msg with
+        | Dummy -> m
+
+    let bindings () : Binding<Model, Msg> list = [
+        "Dummy" |> Binding.cmd Dummy
+    ]
+
+[<EntryPoint; STAThread>]
+let main _ =
+    Program.mkSimpleWpf App.init App.update App.bindings
+    |> Program.withConsoleTrace
+    |> Program.runWindowWithConfig
+        { ElmConfig.Default with LogConsole = true; Measure = true }
+        (Views.MainWindow())
