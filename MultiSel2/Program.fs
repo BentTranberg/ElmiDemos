3326d4a1f4652f80f8da5df8b06f96decf3d6785
Bent Tranberg <bent.tranberg@gmail.com>
2019-11-24 08:11:00 +0100

    Project MultiSel2 is the second attempt at getting ListBox with SelectionMode="Extended" to work.

---
+++ MultiSel2/Program.fs
@@ -0,0 +1,64 @@
+﻿module MultiSel2.Program
+
+open System
+open Elmish
+open Elmish.WPF
+
+module App =
+
+    type Line =
+        {
+            Id: Guid
+            Marked: bool
+            DisplayText: string
+        }
+
+    type Model =
+        {
+            Lines: Line list
+        }
+
+    let init () =
+        let line marked text = { Id = Guid.NewGuid(); Marked = marked; DisplayText = text }
+        {
+            Lines = [
+                line false "Zero"
+                line true "One"
+                line true "Two"
+                line false "Three"
+                line true "Four"
+                line true "Five"
+                line false "Six"
+            ]
+        }
+
+    type Msg =
+        | SetMarked of Guid * bool
+
+    let update msg m =
+        match msg with
+        | SetMarked (id, marked) ->
+            let lines = m.Lines |> List.map (fun line ->
+                if line.Id = id then { line with Marked = marked } else line)
+            { m with Lines = lines }
+
+    let bindings () : Binding<Model, Msg> list = [
+        "Lines" |> Binding.subModelSeq((fun m -> m.Lines), (fun line -> line), fun () ->
+            [
+                "Id" |> Binding.oneWay (fun (_, line) -> line.Id)
+                "Marked" |> Binding.twoWay (
+                    (fun (m, line) -> line.Marked),
+                    (fun marked (m, line) -> SetMarked (line.Id, marked))
+                    )
+                "DisplayText" |> Binding.oneWay (fun (_, line) ->
+                    line.DisplayText + (if line.Marked then " is checked" else " is not checked"))
+            ])
+    ]
+
+[<EntryPoint; STAThread>]
+let main _ =
+    Program.mkSimpleWpf App.init App.update App.bindings
+    |> Program.withConsoleTrace
+    |> Program.runWindowWithConfig
+        { ElmConfig.Default with LogConsole = true; Measure = true }
+        (Views.MainWindow())
